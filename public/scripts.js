0c892af7e1c6038d5de30c2673fef59865c64063
Noah Hicks <noahg@ad.unc.edu>
2023-05-01 22:40:16 -0400

    Progress condition html

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,76 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+
+function onLoad(){
+    for(let e of document.getElementsByName('rpsls')){
+        e.style.display =  "none"
+    }
+
+    for(let e of document.getElementsByName('player2')){
+        e.style.display =  "none"
+    }
+
+}
+
+function getGameType(){
+    
+}
+
+function rpsls(){
+    let type = 'rps'
+    for (let x of document.getElementsByName("game")) {
+        if (x.checked == true) { type = x.id }
+    }
+    if(type == 'rpsls'){
+        for(let e of document.getElementsByName('rpsls')){
+            e.style.display =  "inline"
+        }
+    }
+}
+
+function rps(){
+    let type = 'rps'
+    for (let x of document.getElementsByName("game")) {
+        if (x.checked == true) { type = x.id }
+    }
+    if(type == 'rps'){
+        for(let e of document.getElementsByName('rpsls')){
+            e.style.display =  "none"
+        }
+    }
+}
+
+function comp(){
+    let player = "comp"
+    for(let p of document.getElementsByName("comp")){
+        if (p.checked == true) { player = p.id }
+    }
+    if(player = 'p2'){
+        for(let t of document.getElementsByName('player2')){
+            t.style.display = "none"
+        }
+    }
+
+}
+function player(){
+    let player = "comp"
+    for(let p of document.getElementsByName("comp")){
+        if (p.checked == true) { player = p.id }
+    }
+    if(player = 'p2'){
+        for(let t of document.getElementsByName('player2')){
+            t.style.display = "inline"
+        }
+    }
+}
+
+function play(){
+    let type = 'rps'
+    for (let x of document.getElementsByName("game")) {
+        if (x.checked == true) { type = x.id }
+    }
+    console.log(type)
+
+}
--- server.js
+++ server.js
@@ -62,7 +62,62 @@ const app = express()
 // Set a port for the server to listen on
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
-//
+
+
+import { rps, rpsls } from "./lib/rpsls.js"
+// import minimist from "minimist"
+// import express from "express"
+
+var argv = minimist(process.argv.slice(2));
+//var app = express()
+
+//const port = argv.port || 5000;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app/', (req, res) => {
+    res.status(200).send("200 OK")
+})
+
+app.get('/app/rps/', (req, res) => { 
+    res.status(200).send(rps(null));
+})
+
+app.get('/app/rpsls/', (req, res) => { 
+    res.status(200).send(rpsls(null));
+})
+
+app.get('/app/rps/play/', (req, res) => { res.status(200).send(rps(req.query.shot)); })
+
+app.post('/app/rps/play/', (req, res) => { res.status(200).send(rps(req.body.shot)); })
+
+app.get('/app/rpsls/play/', (req, res) => {  
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+app.post('/app/rpsls/play/', (req, res) => { 
+    res.status(200).send(rpsls(req.body.shot));
+})
+
+app.get('/app/rps/play/:shot/', (req, res) => {
+    res.status(200).send(rps(req.params.shot));
+})
+
+app.get('/app/rpsls/play/:shot/', (req, res) => { 
+   res.status(200).send(rpsls(req.params.shot)) ;
+})
+
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+
 // Create and update access log
 // The morgan format below is the Apache Foundation combined format but with ISO8601 dates
 app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
