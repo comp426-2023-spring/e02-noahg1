50ba98101b64d17088afdaed9de20dd134aae117
Noah Hicks <noahg@ad.unc.edu>
2023-05-02 14:14:27 -0400

    Some sstyling and function

--- public/scripts.js
+++ public/scripts.js
@@ -4,18 +4,54 @@
 
 
 function onLoad(){
+    document.getElementById('comp').checked = false;
+    document.getElementById('rock').checked = true;
+    document.getElementById("results").style.display = "none"
+    document.getElementById("shotImg").style.display = "none"
+    document.getElementById("oppShotImg").style.display = "none"
+    document.getElementById("shotImgLab").style.display = "none"
+    document.getElementById("oppShotImgLab").style.display = "none"
     for(let e of document.getElementsByName('rpsls')){
         e.style.display =  "none"
     }
 
-    for(let e of document.getElementsByName('player2')){
+    for(let e of document.getElementsByName('shots')){
+        e.style.display =  "none"
+    }
+}
+
+function reset(){
+    document.getElementById('comp').checked = false;
+    document.getElementById('rpsls').checked = true;
+    document.getElementById('rpsls').checked = false;
+    document.getElementById("results").style.display = "none"
+    document.getElementById("shotImg").style.display = "none"
+    document.getElementById("oppShotImg").style.display = "none"
+    document.getElementById("shotImgLab").style.display = "none"
+    document.getElementById("oppShotImgLab").style.display = "none"
+    for(let e of document.getElementsByName('rpsls')){
         e.style.display =  "none"
     }
 
+    for(let e of document.getElementsByName('shots')){
+        e.style.display =  "none"
+    }
 }
 
-function getGameType(){
+function rules(){
+    window.alert(`Rules:
+    Scissors CUTS Paper
+    Paper COVERS Rock
+    Rock SMOOSHES Lizard
+    Lizard POISONS Spock
+    Spock SMASHES Scissors
+    Scissors DECAPITATES Lizard
+    Lizard EATS Paper
+    Paper DISPROVES Spock
+    Spock VAPORIZES Rock
+    Rock CRUSHES Scissors
     
+    `)
 }
 
 function rpsls(){
@@ -43,34 +79,68 @@ function rps(){
 }
 
 function comp(){
-    let player = "comp"
-    for(let p of document.getElementsByName("comp")){
-        if (p.checked == true) { player = p.id }
-    }
-    if(player = 'p2'){
-        for(let t of document.getElementsByName('player2')){
-            t.style.display = "none"
+    let box = document.getElementById('comp').checked
+
+    if(box){
+        for(let x of document.getElementsByName('shots')){
+            x.style.display = "inline"
+        }
+    } else {
+        for(let x of document.getElementsByName('shots')){
+            x.style.display = "none"
         }
     }
 
 }
-function player(){
-    let player = "comp"
-    for(let p of document.getElementsByName("comp")){
-        if (p.checked == true) { player = p.id }
-    }
-    if(player = 'p2'){
-        for(let t of document.getElementsByName('player2')){
-            t.style.display = "inline"
-        }
-    }
-}
 
-function play(){
+async function play(){
     let type = 'rps'
+
     for (let x of document.getElementsByName("game")) {
         if (x.checked == true) { type = x.id }
     }
-    console.log(type)
+    
+    let shot = 'rock'
+
+    for(let x of document.getElementsByName('throw')){
+        if (x.checked == true) {shot = x.id}
+    }
+
+    let opp = 'comp'
+
+    for (let x of document.getElementsByName('comp')){
+        if(x.checked == true){opp = x.id}
+    }
+
+
+    let baseurl = window.location.href.concat('app/')
+    let url = baseurl.concat(type.concat('/play/'))
+
+    if(document.getElementById('comp').checked){
+        url = url.concat(shot)
+    }
+
+    let response = await fetch(url)
+    let result = await response.json()
+    console.log(result)
+
+    resultString = 'You selected ' + result.player
+
+    
+    document.getElementById("shotImg").setAttribute("src", "img/"+result.player+".jpg");
+    document.getElementById("shotImgLab").style.display = "block"
+    document.getElementById("shotImg").style.display = "inline"
+    document.getElementById("oppShotImg").style.display = "none"
+    document.getElementById("oppShotImgLab").style.display = "none"
+
+    if (document.getElementById('comp').checked) {
+        resultString = resultString + ' , your opponent selected ' + result.opponent + '. Result: ' + result.result;
+        document.getElementById("oppShotImg").setAttribute("src", "img/"+result.opponent+".jpg");
+        document.getElementById("oppShotImgLab").style.display = "block"
+        document.getElementById("oppShotImg").style.display = "inline"
+    }
+
+    document.getElementById("results").innerText = resultString
+    document.getElementById("results").style.display = "block"
 
 }
--- server.js
+++ server.js
@@ -63,59 +63,48 @@ const app = express()
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
 
-
 import { rps, rpsls } from "./lib/rpsls.js"
-// import minimist from "minimist"
-// import express from "express"
-
-var argv = minimist(process.argv.slice(2));
-//var app = express()
-
-//const port = argv.port || 5000;
-
-app.use(express.json())
+// Parsing requests with url request
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/app/', (req, res) => {
-    res.status(200).send("200 OK")
+app.get('/app/', (req, res) => {     
+    res.status(200).send("200 OK");
 })
 
-app.get('/app/rps/', (req, res) => { 
+app.get('/app/rps/', (req, res) => {     
     res.status(200).send(rps(null));
 })
 
-app.get('/app/rpsls/', (req, res) => { 
+app.get('/app/rpsls/', (req, res) => {  
     res.status(200).send(rpsls(null));
 })
 
-app.get('/app/rps/play/', (req, res) => { res.status(200).send(rps(req.query.shot)); })
+app.get('/app/rps/play/', (req, res) => {   
+    res.status(200).send(rps(req.query.shot));
+})
 
-app.post('/app/rps/play/', (req, res) => { res.status(200).send(rps(req.body.shot)); })
+app.post('/app/rps/play/', (req, res) => {   
+    res.status(200).send(rps(req.body.shot));
+})
 
-app.get('/app/rpsls/play/', (req, res) => {  
+app.get('/app/rpsls/play/', (req, res) => {   
     res.status(200).send(rpsls(req.query.shot));
 })
 
-app.post('/app/rpsls/play/', (req, res) => { 
+app.post('/app/rpsls/play/', (req, res) => {   
     res.status(200).send(rpsls(req.body.shot));
 })
 
-app.get('/app/rps/play/:shot/', (req, res) => {
+app.get('/app/rps/play/:shot/', (req, res) => {     
+   
     res.status(200).send(rps(req.params.shot));
 })
 
-app.get('/app/rpsls/play/:shot/', (req, res) => { 
-   res.status(200).send(rpsls(req.params.shot)) ;
+app.get('/app/rpsls/play/:shot/', (req, res) => {   
+    
+    res.status(200).send(rpsls(req.params.shot));
 })
-
-
-app.get('*', (req, res) => {
-    res.status(404).send('404 NOT FOUND')
-})
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
 
 
 // Create and update access log
